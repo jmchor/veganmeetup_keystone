11d949401bcc16dbb9403aff59904548bf62760f
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-19 08:03:18 +0200

    set up image list

---
+++ image.ts
@@ -0,0 +1,33 @@
+import { cloudinaryImage } from '@keystone-6/cloudinary';
+import { list } from '@keystone-6/core';
+import { allowAll } from '@keystone-6/core/access';
+import { text } from '@keystone-6/core/fields';
+import 'dotenv/config';
+
+export const cloudinary = {
+	cloudName: process.env.CLOUDINARY_CLOUD_NAME as string,
+	apiKey: process.env.CLOUDINARY_KEY as string,
+	apiSecret: process.env.CLOUDINARY_SECRET as string,
+	folder: process.env.CLOUDINARY_API_FOLDER as string,
+};
+
+export const Image = list({
+	access: allowAll,
+
+	fields: {
+		image: cloudinaryImage({
+			cloudinary,
+			label: 'Source',
+		}),
+
+		alt: text({
+			label: 'Description',
+		}),
+	},
+
+	ui: {
+		listView: {
+			initialColumns: ['image', 'alt'],
+		},
+	},
+});
