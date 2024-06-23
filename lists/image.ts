2aec4ec1748ed854d059ee532563c06f5a01016d
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-23 07:02:37 +0200

    add image category

--- lists/image.ts
+++ lists/image.ts
@@ -1,7 +1,7 @@
 import { cloudinaryImage } from '@keystone-6/cloudinary';
 import { list } from '@keystone-6/core';
 import { allowAll } from '@keystone-6/core/access';
-import { text } from '@keystone-6/core/fields';
+import { select, text } from '@keystone-6/core/fields';
 import 'dotenv/config';
 
 export const cloudinary = {
@@ -22,6 +22,23 @@ export const Image = list({
 
 		alt: text({
 			label: 'Description',
+			ui: {
+				itemView: { fieldMode: 'edit' },
+			},
+		}),
+
+		category: select({
+			options: [
+				{ label: 'Masonry', value: 'masonry' },
+				{ label: 'Articles', value: 'articles' },
+				{ label: 'Activists', value: 'activists' },
+				{ label: 'Podcasts', value: 'podcasts' },
+				{ label: 'Books', value: 'books' },
+				{ label: 'Videos', value: 'videos' },
+				{ label: 'Timeline', value: 'timeline' },
+				{ label: 'General', value: 'general' },
+			],
+			defaultValue: 'background',
 			ui: {
 				itemView: { fieldMode: 'edit' },
 			},
