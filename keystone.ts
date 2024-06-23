c3830f50d0a75d717127ac96659bbaa3799b85b8
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-23 07:02:26 +0200

    update config

--- keystone.ts
+++ keystone.ts
@@ -2,8 +2,14 @@ import { config } from '@keystone-6/core';
 import { lists } from './schema';
 import { session, withAuth } from './auth';
 
-export default withAuth(
-	config({
+export default config(
+	withAuth({
+		server: {
+			cors: {
+				origin: ['http://localhost:5173'],
+				credentials: true,
+			},
+		},
 		db: {
 			provider: 'postgresql',
 			url: 'postgresql://me:password@localhost:5432/veganmeetup',
