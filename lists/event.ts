394db7c19d969ee50ddd8210d21e3269a94b1393
Johannes Chorzempa <jchorzempa@posteo.org>
2024-06-23 07:45:58 +0200

    added document field to events

--- lists/event.ts
+++ lists/event.ts
@@ -1,6 +1,7 @@
 import { list } from '@keystone-6/core';
 import { allowAll } from '@keystone-6/core/access';
 import { calendarDay, checkbox, text, timestamp } from '@keystone-6/core/fields';
+import { document } from '@keystone-6/fields-document';
 
 export const Event = list({
 	access: allowAll,
@@ -10,11 +11,17 @@ export const Event = list({
 		date: calendarDay({ label: 'Date' }),
 		pastEvent: checkbox({ label: 'Is this a past event?' }),
 		location: text({ label: 'Location', ui: { itemView: { fieldMode: 'edit' } } }),
-		content: text({
-			label: 'Content',
-			ui: {
-				itemView: { fieldMode: 'edit' },
-			},
+		content: document({
+			formatting: true,
+			layouts: [
+				[1, 1],
+				[1, 1, 1],
+				[2, 1],
+				[1, 2],
+				[1, 2, 1],
+			],
+			links: true,
+			dividers: true,
 		}),
 		timestamp: timestamp({ defaultValue: { kind: 'now' } }),
 	},
